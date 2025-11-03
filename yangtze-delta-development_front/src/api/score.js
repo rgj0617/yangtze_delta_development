a25a79b13bbb68d35a9b4ec22dcf104000e9ed54
Roor Li <1563856124@qq.com>
2025-11-03 15:39:35 +0800

    api fix

--- yangtze-delta-development_front/src/api/score.js
+++ yangtze-delta-development_front/src/api/score.js
@@ -1,15 +1,15 @@
 import { get, post } from "index.js"
 
-const getScoreRanking = function(year) {
-  get(`/score/ranking/${year}`)
-}
+export default class {
+  getScoreRanking(year) {
+    get(`/score/ranking/${year}`)
+  }
 
-const getDimensionScore = function(year) {
-  get(`/score/dimension/${year}`)
-}
+  getDimensionScore(year) {
+    get(`/score/dimension/${year}`)
+  }
 
-const getNormalizeIndicators = function(year) {
-  get(`/score/indicators/${year}`)
+  getNormalizeIndicators(year) {
+    get(`/score/indicators/${year}`)
+  }
 }
-
-export { getScoreRanking, getDimensionScore, getNormalizeIndicators }
